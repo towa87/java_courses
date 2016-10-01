5bad62746482e25e2c2e4e9b925beab36cbf0bd6
towa87 <a.krokhmalev@gmail.com>
2016-10-01 21:38:40 +0200

    задание 14 и 15 лекция 6

--- addressbookWebTests/src/test/java/javaForTesters/tests/UserCreationTest.java
+++ addressbookWebTests/src/test/java/javaForTesters/tests/UserCreationTest.java
@@ -3,7 +3,6 @@ package javaForTesters.tests;
 import com.thoughtworks.xstream.XStream;
 import javaForTesters.model.AccountCreation;
 import javaForTesters.model.Accounts;
-import javaForTesters.model.GroupData;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -41,22 +40,13 @@ public class UserCreationTest extends TestBase {
     app.goTo().homePage();
     Accounts before = app.user().userList();
     app.goTo().creationUserPage();
-   /*  File photo = new File("addressbookWebTests/src/test/resources/test.png");
-   AccountCreation account = new AccountCreation().withName("Ivan")
-            .withLastname1("Smit").withNick("ST123").withNick("User")
-            .withCompany("Software").withTelephoneHome("+474888822")
-            .withEmail2("test@test2.com").withMobilePhone("+474888821")
-            .withEmail3("test@test3.com").withWorkPhone("+474888821").withAddress("St.Ivanova")
-            .withEmail("test@test.com").withHomepage("localhost:8080/")
-            .withAyear("1990").withBirthday("2000").withGroup("test3").withPhoto(photo); */
+
     app.user().createUser(account, true);
     app.goTo().homePage();
     assertThat(app.user().count(), equalTo(before.size() + 1));
     Accounts after = app.user().userList();
 
-    //assertThat(after.size(), equalTo(before.size() + 1));
-
-    assertThat(after, equalTo(before.withAdded(account.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+      assertThat(after, equalTo(before.withAdded(account.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
   @Test(enabled = false)
