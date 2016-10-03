ddc703a8cfdd3782a02efb9e27f23771ca9687bd
towa87 <a.krokhmalev@gmail.com>
2016-10-03 08:58:28 +0200

    6.10 не возможно запустить тесты с консоли

--- addressbookWebTests/src/test/java/javaForTesters/appManager/ApplicationManager.java
+++ addressbookWebTests/src/test/java/javaForTesters/appManager/ApplicationManager.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+
+import java.io.IOException;
 import java.util.Properties;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,7 +18,7 @@ import java.util.concurrent.TimeUnit;
  * Created by Антон on 21.08.2016.
  */
 public class ApplicationManager {
-  //private final Properties properties;
+  private final Properties properties;
     private String browser;
 
   WebDriver wd;
@@ -27,15 +29,15 @@ public class ApplicationManager {
   private UserHelper userHelper;
 
   public ApplicationManager(String browser)  {
-   // properties = new Properties();
+    properties = new Properties();
     this.browser = browser;
 
   }
 
 
-  public void init()  {
-   // String target = System.getProperty("target", "local");
-   // properties.load(new FileReader( new File(String.format("scr/test/resources/$s.properties", target))));
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader( new File(String.format("addressbookWebTests/src/test/resources/%s.properties", target))));
    // String browser = BrowserType.FIREFOX;
     if (browser.equals(BrowserType.FIREFOX))
     {
@@ -45,14 +47,14 @@ public class ApplicationManager {
     if (browser.equals(BrowserType.IE))
     {wd = new InternetExplorerDriver();}
     wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    //wd.get(properties.getProperty("web.baseUrl"));
-    wd.get("http://localhost:8080/addressbook/birthdays.php");
+    wd.get(properties.getProperty("web.baseURl"));
+    //wd.get("http://localhost:8080/addressbook/birthdays.php");
     groupsHelper = new GroupsHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     userHelper = new UserHelper(wd);
-    sessionHelper.login("admin", "secret");
-    //sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+    // sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
 
--- addressbookWebTests/src/test/java/javaForTesters/tests/TestBase.java
+++ addressbookWebTests/src/test/java/javaForTesters/tests/TestBase.java
@@ -10,8 +10,8 @@ import org.testng.annotations.BeforeSuite;
  */
 public class TestBase {
 
- // protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+ protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+  //protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeSuite
   public void setUp() throws Exception {
