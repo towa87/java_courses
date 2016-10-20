4d6b8d14a117300e7f6fab9d2d01eb1e2b3d839c
towa87 <a.krokhmalev@gmail.com>
2016-10-20 14:28:52 +0200

    ошибка прав доступа видео 5 лекция 8

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -2,6 +2,10 @@
 $g_hostname = 'localhost';
 $g_db_type = 'mysql';
 $g_database_name = 'bugtracker';
-$g_username = 'root';
+$g_db_username = 'root';
 $g_db_password = '';
 $g_singup_use_captcha = OFF;
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'DShL9IbE7F0KbR2Bp/8uoymnfUh1ffBdUOoCQSiJg1Y=';
